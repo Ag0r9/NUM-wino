9efad82a9e4398ce81d40972535277b12f5ae97e
Adam Gorgon <Ag0r@protonmail.com>
2023-06-13 20:51:26 +0200

    fix: small fixes :)

--- analytics/utils.py
+++ analytics/utils.py
@@ -8,8 +8,6 @@ from src.utils import read_csv, save_csv
 def calculate_wine_price():
     paths: Paths = Paths()
     data = read_csv(paths.INPUT_DIR / "WineQT.csv")
-    random_numbers = np.random.normal(loc=0.5, scale=0.1, size=data.shape[0]) / 2
-    random_numbers = np.clip(random_numbers, 0, 1)
-    data["price"] = random_numbers * data["quality"] * 20
+    data["price"] = (data["quality"] * 10) + (np.random.rand(data.shape[0]) * 10 )
     data["price"] = data["price"].round(2)
     save_csv(data.loc[:, ["Id", "price"]], paths.INPUT_DIR / "WinePrices.csv")
